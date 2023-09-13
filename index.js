79379a53345a802bba24251eb215228d4b7646c1
Muhammad Muneeb <64094867+mmuneeeb@users.noreply.github.com>
2023-09-13 12:13:52 -0400

    updated styling

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ let width = 800;
 let height = 600;
 let padding = 40;
 
-let barHeight
-let x 
-let xAxis
-let yaxis
+let heightScale
+let xScale 
+let xAxisScale
+let yAxisScale
 
 let svg = d3.select('svg')
 
@@ -21,33 +21,55 @@ let drawCanvas = () => {
 }
 
 let generateScales = () => {
-    barHeight = d3.scaleLinear()
+    heightScale = d3.scaleLinear()
                   .domain([0, d3.max(parsed_data, (d) => {return d[1]})])
                   .range([0, height - 2*padding])
 
-    x = d3.scaleLinear()
-          .domain([0, parsed_data.length - 1])
-          .range([padding, width - padding])
+    xScale = d3.scaleLinear()
+                  .domain([0, parsed_data.length - 1])
+                  .range([padding, width - padding])
 
     let datesArray = parsed_data.map(d => {return new Date(d[0])})
-    xAxis = d3.scaleTime()
-              .domain([d3.min(datesArray), d3.max(datesArray)])
-              .range([padding, width - padding])
+    xAxisScale = d3.scaleTime()
+                  .domain([d3.min(datesArray), d3.max(datesArray)])
+                  .range([padding, width - padding])
 
-    yaxis = d3.scaleLinear()
-              .domain([0, d3.max(parsed_data, (d) => {return d[1]})])
-              .range([height - padding, padding])
+    yAxisScale = d3.scaleLinear()
+                  .domain([0, d3.max(parsed_data, (d) => {return d[1]})])
+                  .range([height - padding, padding])
 }
 
 let drawBars = () => {
-
+   svg.selectAll('rect')
+      .data(parsed_data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('width', ((width-(2*padding))/parsed_data.length))
+      .attr("height",(d => {return heightScale(d[1])}))
+      .attr('x', ((d, i) => {return xScale(i)}))
+      .attr('y', ((d) => {return height - padding - heightScale(d[1])}))
+      .attr('data-date', (d => {return d[0]}))
+      .attr('data-gdp', (d => {return d[1]}))
+      .append("title")
+      .text(d => { return "Year: " + d[0] + "\n" + "GDP: " + d[1] + " million"})
+      .attr('id', 'tooltip')
+      
 }
 
 let generateAxis = () => {
-    let cisca = d3.axisBottom(xAxis)
+    let xAxis = d3.axisBottom(xAxisScale)
     svg.append('g')
-       .call(x)
+       .call(xAxis)
        .attr('id', 'x-axis')
+       .attr('transform', 'translate(0,' + (height - padding) + ')')
+
+   let yAxis = d3.axisLeft(yAxisScale)
+   svg.append('g')
+      .call(yAxis)
+      .attr('id', 'y-axis')
+      .attr('transform', 'translate(' + padding + ', 0)')
+
     
 }
 
